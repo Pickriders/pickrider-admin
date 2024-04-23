b9e6ef28710a11034f2f0de87d6499f07b70d058
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2024-04-23 12:32:39 +0100

    add buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/common/Button";
 import { Heading } from "@/components/common/heading";
 
 export default function Home() {
   return (
     <main>
       <Heading variant="h1">Dashboard</Heading>
+      <Button variant="business">Register</Button>
     </main>
   );
 }
