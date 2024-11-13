4c6a656289d620e9499425582e932da15fcc1227
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2024-11-13 10:16:41 +0100

    fix: dropdown close btn

--- src/app/dashboard/FilterByDate/index.tsx
+++ src/app/dashboard/FilterByDate/index.tsx
@@ -7,11 +7,12 @@ import { format } from "date-fns";
 
 export const FilterDyDate = () => {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
+  const [open, setOpen] = React.useState(false);
 
   const formattedDate = format(date ?? new Date(), "MMM d, yyyy");
 
   return (
-    <UI.Popover>
+    <UI.Popover open={open} onOpenChange={setOpen}>
       <UI.PopoverTrigger asChild>
         <button className="font-clash-display flex items-center gap-x-3 text-sm font-semibold rounded-lg text-primary-gray px-4 bg-background py-1.5">
           {formattedDate}
@@ -19,9 +20,14 @@ export const FilterDyDate = () => {
         </button>
       </UI.PopoverTrigger>
       <UI.PopoverContent side="bottom" className="mr-12 w-[23rem]">
-        <p className="font-clash-display font-semibold mb-3 text-primary-gray text-sm">
-          Pick a date
-        </p>
+        <div className="mb-5 flex items-center justify-between">
+          <p className="font-clash-display font-semibold  text-primary-gray text-sm">
+            Pick a date
+          </p>
+          <button onClick={() => setOpen(false)}>
+            <SVG.CloseIcon />
+          </button>
+        </div>
         <UI.Calendar
           mode="single"
           selected={date}
