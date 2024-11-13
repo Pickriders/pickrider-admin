e473fbc8c2f69413a739998fba3bbcbae3eb2851
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2024-11-13 13:37:38 +0100

    fix: file nameing

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -5,7 +5,7 @@ import { Notification } from "./Notification";
 
 export const Header = () => {
   return (
-    <header className="h-[6.2rem] py-3 pl-6 pr-9 bg-background sticky top-0 z-[100] border-b  flex items-center justify-between">
+    <header className="h-[6.2rem] py-3 pl-6 pr-9 bg-background sticky top-0 z-30 border-b  flex items-center justify-between">
       <Link href={"/dashboard"} className=" pt-2 inline-block mb-0">
         <SVG.LogoIcon />
       </Link>
