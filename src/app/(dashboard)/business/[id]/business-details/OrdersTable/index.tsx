0a4041815615c5c53b94aeacc51a81c7fb97986b
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2024-12-05 13:10:12 +0100

    fix: buinsess order table

--- src/app/(dashboard)/business/[id]/business-details/OrdersTable/index.tsx
+++ src/app/(dashboard)/business/[id]/business-details/OrdersTable/index.tsx
@@ -8,7 +8,7 @@ export const OrdersTable = () => {
         <span className="w-full flex-1 h-[1px] bg-gray-200 dark:bg-gray-100/20" />
       </div>
 
-      <div className="scroll-bar overflow-y-auto h-[18rem]">
+      <div>
         <UI.Table>
           <UI.TableHeader className="bg-transparent">
             <UI.TableRow>
@@ -20,30 +20,34 @@ export const OrdersTable = () => {
               <UI.TableHead>Order Type</UI.TableHead>
             </UI.TableRow>
           </UI.TableHeader>
-          <UI.TableBody>
-            {Array(10)
-              .fill(0)
-              .map((_, i) => {
-                return (
-                  <UI.TableRow key={i}>
-                    <UI.TableCell>1.</UI.TableCell>
-                    <UI.TableCell>#43650</UI.TableCell>
-                    <UI.TableCell>ENU-225-AE</UI.TableCell>
-                    <UI.TableCell>
-                      <span className="text-[#DBAD0E] font-faktum-test px-2.5 py-1.5 bg-[#F9C6131F] rounded-full">
-                        Processing
-                      </span>
-                    </UI.TableCell>
-                    <UI.TableCell>
-                      <span>9:42pm</span> <br />
-                      <span>12/04/23</span>
-                    </UI.TableCell>
-                    <UI.TableCell>Pickup Order</UI.TableCell>
-                  </UI.TableRow>
-                );
-              })}
-          </UI.TableBody>
         </UI.Table>
+        <div className="scroll-bar overflow-y-auto h-[15rem]">
+          <UI.Table>
+            <UI.TableBody>
+              {Array(10)
+                .fill(0)
+                .map((_, i) => {
+                  return (
+                    <UI.TableRow key={i}>
+                      <UI.TableCell>1.</UI.TableCell>
+                      <UI.TableCell>#43650</UI.TableCell>
+                      <UI.TableCell>ENU-225-AE</UI.TableCell>
+                      <UI.TableCell>
+                        <span className="text-[#DBAD0E] font-faktum-test px-2.5 py-1.5 bg-[#F9C6131F] rounded-full">
+                          Processing
+                        </span>
+                      </UI.TableCell>
+                      <UI.TableCell>
+                        <span>9:42pm</span> <br />
+                        <span>12/04/23</span>
+                      </UI.TableCell>
+                      <UI.TableCell>Pickup Order</UI.TableCell>
+                    </UI.TableRow>
+                  );
+                })}
+            </UI.TableBody>
+          </UI.Table>
+        </div>
       </div>
       <div className="flex justify-end mt-5">
         <UI.PaginationBtns totalPages={4} currentPage={1} />
