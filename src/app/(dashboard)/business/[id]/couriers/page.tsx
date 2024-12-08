46140153d53127d2b33732f3c9e4a3c26e8671b6
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2024-12-08 10:45:02 +0100

    add: business couriers page & components (#49)
    * add: business couriers page & components
    
    * refactor: active link logic

---
+++ src/app/(dashboard)/business/[id]/couriers/page.tsx
@@ -0,0 +1,44 @@
+import { UI } from "@/components/ui";
+import { notFound } from "next/navigation";
+import { CouriersTable } from "./CouriersTable";
+import { couriersTableColumn } from "./CouriersTableColumn";
+import { Suspense } from "react";
+import { SuspendCourierModal } from "./SuspendModal";
+import { DeleteCourierModal } from "./DeleteModal";
+
+export async function generateStaticParams() {
+  return Array(20)
+    .fill(0)
+    .map((_, id) => ({
+      id: `${id}`,
+    }));
+}
+
+const BusinessCourierPage = ({ params }: { params: { id: string } }) => {
+  if (!params.id) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <UI.BreadCrumbNav
+        pageLinks={[
+          { href: "/business", label: "Business" },
+          { href: "business-details", label: "Peterson Corp" },
+        ]}
+        currentPage="Courier"
+        rootPageLink="/business"
+      />
+      <div className="mt-10">
+        <CouriersTable columns={couriersTableColumn} data={Array(13).fill(0)} />
+      </div>
+
+      {/* Modals */}
+      <Suspense>
+        <SuspendCourierModal />
+        <DeleteCourierModal />
+      </Suspense>
+    </div>
+  );
+};
+export default BusinessCourierPage;
--- src/components/ui/TableUser/index.tsx
+++ src/components/ui/TableUser/index.tsx
@@ -1,16 +1,16 @@
 interface TableUser {
   name: string;
   // img:string;
-  // email:string
+  subText?: string;
 }
 
-export const TableUser = ({ name }: TableUser) => {
+export const TableUser = ({ name, subText }: TableUser) => {
   return (
     <div className="flex items-center gap-x-2">
       <div className="size-[1.7rem] shrink-0 rounded-full bg-muted/70"></div>
       <div className="text-nowrap">
         <p className="font-semibold font-faktum-test">{name}</p>
-        <span></span>
+        <span>{subText}</span>
       </div>
     </div>
   );
