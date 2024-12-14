e37b9985a813c587db1d1e86961b760c31ec4609
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2024-12-14 21:02:26 +0100

    fix: next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.node$/,
-      use: "node-loader",
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
