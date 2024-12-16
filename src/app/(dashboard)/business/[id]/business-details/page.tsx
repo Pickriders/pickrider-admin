ee7efa7e720d88fc54a3b9004c60f8a367845fcf
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2024-12-16 16:17:01 +0100

    Update page.tsx

--- src/app/(dashboard)/business/[id]/business-details/page.tsx
+++ src/app/(dashboard)/business/[id]/business-details/page.tsx
@@ -39,7 +39,7 @@ const BusinessDetailsPage = ({ params }: { params: { id: string } }) => {
             >
               <ShieldCheck />
               <span className="font-montserrat text-xs font-semibold">
-                couriers verification
+               Business Verification
               </span>
             </Link>
             <Link
