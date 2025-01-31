9e9063878b09b58c8765c7c8b0aff56defc6c2c6
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-01-31 17:59:20 +0100

    fix:data undefined potential bug

--- src/components/CouriersTable/index.tsx
+++ src/components/CouriersTable/index.tsx
@@ -12,7 +12,7 @@ import { DataTable } from "./DataTable";
 import { ListUserResponseDto } from "@/services";
 
 interface DataTableProps {
-  data: ListUserResponseDto | undefined;
+  data?: ListUserResponseDto | undefined;
   isLoading: boolean;
 }
 
