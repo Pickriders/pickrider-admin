2bfe94aaceab7ad3ae10ca2f858ae590ae9c7b0a
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-02-04 06:50:56 +0100

    feat/ user key update

--- src/api/queries/user.ts
+++ src/api/queries/user.ts
@@ -6,13 +6,14 @@ import { ColumnDef } from "@tanstack/react-table";
 import { useSearchParams } from "next/navigation";
 
 export const USER_KEY = {
+  PROFILE: "profile",
   USER: "user",
   USERS: "users",
 };
 
 export const useGetUserQuery = () =>
   useApiQuery({
-    queryKey: [USER_KEY.USER],
+    queryKey: [USER_KEY.PROFILE],
     queryFn: () => apiService.getUserProfile(),
   });
 
@@ -24,7 +25,7 @@ export const useGetUsersQuery = (query: GetUsersParams) =>
 
 export const useGetUserDetailsQuery = (userId: string) =>
   useApiQuery({
-    queryKey: [userId],
+    queryKey: [USER_KEY.USER, userId],
     queryFn: () => apiService.getUser(userId),
   });
 
