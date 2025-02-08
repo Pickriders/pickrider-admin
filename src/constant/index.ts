8d02c133a8afe8205f8915a38c26109bd4735968
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-02-08 12:55:06 +0100

    feat/ changed api baseURL

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,6 @@
 export * from "./select";
 
-export const BASE_URL = "https://pickriders-api-nest-dev.onrender.com"; // TODO: Replace this with url from env instead
+export const BASE_URL = "https://pickriders-api-dev.onrender.com"; // TODO: Replace this with url from env instead
 
 export const API_URL = `${BASE_URL}/api/v1`;
 
