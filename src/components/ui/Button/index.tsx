5e94ec9eccd87c175f7e031b1c353aa65373e145
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-02-10 18:50:33 +0100

    fix: button rendering (#87)

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -41,7 +41,7 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, loadingText, isLoading, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, loadingText, isLoading, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
@@ -50,10 +50,12 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         disabled={props.disabled || isLoading}
         {...props}
       >
-        {isLoading && (
+        {isLoading ? (
           <div className="flex items-center gap-x-1">
             <LoaderCircle size={15} className="animate-spin" /> {loadingText ?? "Loading..."}
           </div>
+        ) : (
+          children
         )}
       </Comp>
     );
