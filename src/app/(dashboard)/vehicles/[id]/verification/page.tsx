df1c0605f8bb409f5a8838376fdb3aee5fb8fa1d
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-02-11 21:31:05 +0100

    feat/add suspend vehicle (#88)
    * feat/add suspend vehicle
    
    * update:added other input

--- src/app/(dashboard)/vehicles/[id]/verification/page.tsx
+++ src/app/(dashboard)/vehicles/[id]/verification/page.tsx
@@ -1,7 +1,7 @@
 import { UI } from "@/components/ui";
 import { notFound } from "next/navigation";
 import { VerificationPanel } from "./VerificationPanel";
-import React from "react";
+import React, { Suspense } from "react";
 
 interface VehicleVerificationPageProps {
   params: { id: string };
@@ -19,7 +19,9 @@ const VerificationPage: React.FC<VehicleVerificationPageProps> = ({ params }) =>
         pageLinks={[{ href: "/vehicles", label: "Vehicles" }]}
       />
       <div className="mt-16">
-        <VerificationPanel vehicleId={params.id} />
+        <Suspense>
+          <VerificationPanel vehicleId={params.id} />
+        </Suspense>
       </div>
     </div>
   );
